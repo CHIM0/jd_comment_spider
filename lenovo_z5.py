9e0aeb030de312f7834739d181dc10a055b93d43
HONGSHEN <1850464485@qq.com>
2018-06-18 12:50:38 +0800

    add root file

---
+++ lenovo_z5.py
@@ -0,0 +1,47 @@
+from urllib import request
+import json
+import re
+
+class Spider():
+    url1='https://sclub.jd.com/comment/productPageComments.action?callback=fetchJSON_comment98vv2789&productId=7621213&score=0&sortType=5&page='
+    url3='&pageSize=10&isShadowSku=0&fold=1'
+    u1 = "D:\\PycharmProject\\spider\\comment\\comment_page_"
+    u3 = ".json"
+
+    def __get_content(self,page):
+        txt = open("D:\\PycharmProject\\spider\\comment\\allcomment.txt", "w")
+        txt.close()
+
+        for i in range(0,page):
+            full_web_url = self.url1 + str(i) + self.url3
+            r = request.urlopen(full_web_url)
+            html = r.read()
+            html = str(html,encoding='gbk')
+            json_data = json.loads(html[26:-2])
+
+            if json_data['comments'] == [] :
+                break
+            full_local_url = self.u1 + str(i + 1) + self.u3
+            file = open(full_local_url, "w")
+            file.write("Comment Page "+str(i+1)+"\n")
+            file.close()
+            index = 0
+            for a in json_data['comments']:
+                index += 1
+                file = open(full_local_url, "a")
+                file.write(str(index)+"\n"+a['content']+"\n\n")
+                txt = open("D:\\PycharmProject\\spider\\comment\\allcomment.txt", "a")
+                txt.write(a['content'])
+            file.close()
+
+        txt.close()
+
+
+
+    def run(self):
+        self.__get_content(10)
+
+spider=Spider()
+spider.run()
+
+
---
+++ word_cloud.py
@@ -0,0 +1,40 @@
+import re
+import jieba
+import pandas as pd
+import numpy
+from wordcloud import WordCloud,ImageColorGenerator,get_single_color_func
+import matplotlib.pyplot as plt
+import matplotlib
+from os import path
+from PIL import Image
+
+matplotlib.rcParams['figure.figsize'] = (10.0, 5.0)
+file1 = open("D:\\PycharmProject\\spider\\comment\\allcomment.txt", 'r')
+xt = file1.read()
+pattern = re.compile(r'[\u4e00-\u9fa5]+')
+filedata = re.findall(pattern, xt)
+xx = ''.join(filedata)
+file1.close()
+#读取文件
+file2 = open("D:\\PycharmProject\\spider\\comment\\allcomment1.txt", 'w')
+file2.write(xx)
+clear = jieba.cut(xx)
+cleared = pd.DataFrame({'clear': list(clear)})
+#print(clear)
+stopwords = pd.read_csv("chineseStopWords.txt", index_col=False, quoting=3, sep="\t", names=['stopword'], encoding='GBK')
+cleared = cleared[~cleared.clear.isin(stopwords.stopword)]
+#print(std)
+#清洗数据
+count_words=cleared.groupby(by=['clear'])['clear'].agg({"num": numpy.size})
+count_words=count_words.reset_index().sort_values(by=["num"], ascending=False)
+#print(count_words)
+#词云展示
+RGB_coloring =numpy.array(Image.open(path.join("RGB.png")))
+image_colors = ImageColorGenerator(RGB_coloring)
+wordcloud=WordCloud(color_func=get_single_color_func('white'),font_path="simhei.ttf",background_color="white",max_font_size=300,height=800,width=1000,random_state=None) #指定字体类型、字体大小和字体颜色
+word_frequence = {x[0]:x[1] for x in count_words.head(400).values}
+wordcloud=wordcloud.fit_words(word_frequence)
+plt.imshow(wordcloud,interpolation="bilinear")
+# plt.imshow(wordcloud.recolor(color_func=get_single_color_func('white')),interpolation="bilinear")
+plt.axis("off")
+plt.show()
